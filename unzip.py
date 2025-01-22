0db475ee8e7054422bc62734bb906d643456de9d
luqizhi666 <78798951+luqizhi666@users.noreply.github.com>
2025-01-22 14:44:35 +0800

    Add files via upload

---
+++ unzip.py
@@ -0,0 +1,26 @@
+import os,sys
+import zipfile
+
+sys.stdout.reconfigure(encoding='utf-8')
+
+def is_zipfile(file_path):
+    return zipfile.is_zipfile(file_path)
+
+def unzip_all(directory):
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.zip'):
+                file_path = os.path.join(root, file)
+                extract_path = os.path.join(root, file[:-4])
+                if is_zipfile(file_path):
+                    with zipfile.ZipFile(file_path, 'r') as zip_ref:
+                        zip_ref.extractall(extract_path)
+                    print(f'Unzipped {file_path} to {extract_path}')
+                    # 递归解压新解压出的文件夹中的压缩包
+                    unzip_all(extract_path)
+                else:
+                    print(f'Skipped {file_path}, not a valid zip file')
+
+if __name__ == "__main__":
+    directory = 'C:/Users/17080/Documents/yswallpaper'  # Change this to your target directory
+    unzip_all(directory)
