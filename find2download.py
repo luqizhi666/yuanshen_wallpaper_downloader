1e8f777f2e45ae8c58891c58560aa09c36f51a20
luqizhi666 <luqizhi666@gmail.com>
2025-06-01 17:50:35 +0800

    wc

--- find2download.py
+++ find2download.py
@@ -2,6 +2,10 @@ import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.edge.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+
 import sys
 import time
 import os
